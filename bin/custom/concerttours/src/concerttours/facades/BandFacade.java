d97ea61b3001c9a9672f05a0846a06cfa6a1502d
Andrew Tsenhlevych <Andrew_Tsenhlevych@epam.com>
2020-10-27 02:59:40 +0200

    extend facade layer

---
+++ bin/custom/concerttours/src/concerttours/facades/BandFacade.java
@@ -0,0 +1,13 @@
+package concerttours.facades;
+
+import concerttours.data.BandData;
+
+import java.util.List;
+
+
+public interface BandFacade
+{
+	BandData getBand(String name);
+
+	List<BandData> getBands();
+}
